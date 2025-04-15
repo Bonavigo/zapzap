0d686f5e90f38ce177b11e0416e362e72d20323c
oliveiravitor32 <oliveiravitor327@gmail.com>
2025-04-15 14:36:26 -0300

    Cria implementação do websocket para o backend

---
+++ backend/src/main/java/com/example/demo/websocket/ChatController.java
@@ -0,0 +1,37 @@
+package com.example.demo.websocket;
+
+import com.example.demo.domain.message.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/chat/public")
+    public ChatMessage sendMessage(
+            @Payload ChatMessage message
+    ) {
+        return message;
+    }
+
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/chat/public")
+    public ChatMessage addUser(
+            @Payload ChatMessage message,
+            SimpMessageHeaderAccessor headerAccessor
+    ) {
+        // Armazena nome de usuário à sua sessão
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+
+        // Altera conteúdo da mensagem de entrada de usuário no servidor
+        message.setContent("Entrou no chat!");
+
+        return message;
+    }
+
+}
